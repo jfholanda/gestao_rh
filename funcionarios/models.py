d6c211b273d8404d16366c08565b5ba1494c3e03
jfholanda <joaofelipeholanda@gmail.com>
2023-09-10 12:03:11 -0300

    criando app de departamentos

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Funcionario: {self.nome}'
