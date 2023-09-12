f74cebafc3c396b44b1c350e4dd2ed0ef6036a93
jfholanda <joaofelipeholanda@gmail.com>
2023-09-11 21:57:16 -0300

    new features

--- documentos/models.py
+++ documentos/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from funcionarios.models import Funcionario
 
 
 class Documento(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.CharField(max_length=100)
+    percente = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return f'Documento: {self.nome}'
--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from empresas.models import Empresa
+from departamentos.models import Departamento
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return f'Funcionario: {self.nome}'
